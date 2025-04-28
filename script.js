b952953d23be7fe8eabdd6644343b4a9a017a431
priyanshu <priyanshurajsk_cse23@its.edu.in>
2025-04-28 22:33:30 +0530

    some changes to the frontend

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
+function previewFile() {
+    const fileInput = document.getElementById("fileInput");
+    const previewImage = document.getElementById("previewImage");
+
+   
+    if (fileInput.files && fileInput.files[0]) {
+      const reader = new FileReader();
+
+     
+      reader.onload = function (e) {
+        previewImage.src = e.target.result;
+        previewImage.classList.remove("hidden"); 
+      };
+
+      reader.readAsDataURL(fileInput.files[0]); 
+    }
+  }
 async function uploadImageAndFetchResult() {
     const fileInput = document.getElementById('fileInput');
     const file = fileInput.files[0];
